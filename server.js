483fa03f86d33f9c1e500d9ed814c124259836a0
PirasSalvatore <torep.92@gmail.com>
2025-03-13 13:00:30 +0100

    add router in server

--- server.js
+++ server.js
@@ -2,7 +2,8 @@ const express = require("express")
 const app = express()
 const port = 3000
 
-
+//import rout
+const postsRouter = require("./routers/posts")
 
 app.listen(port, () => {
     console.log(`the blog server is run to port http://localhost:${port}`);
@@ -12,3 +13,5 @@ app.get('/', (req, res) => {
     res.send("welcome to blog server")
 })
 
+//uso le rout importate
+app.use("/api/v1/posts".postsRouter)
